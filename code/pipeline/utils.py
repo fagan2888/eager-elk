0cda8e8b31f56cdb277b619d0167672cc23c0f44
Marc Verhagen <marc@cs.brandeis.edu>
2019-12-16 21:14:57 -0500

    Added conversion to lif ; some refactoring.

---
+++ code/pipeline/utils.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+
+def time_elapsed(fun):
+    """Function to be used as a decorator for measuring time elapsed."""
+    def wrapper(*args, **kwargs):
+        t0 = time.time()
+        fun(*args, **kwargs)
+        print("\nTime elapsed = %s" % (time.time() - t0))
+    return wrapper
+
+
+def elements(filelist, start, end):
+    """Generator over the lines in filelist, only yielding lines from line niumber
+    start up to an dincluding end."""
+    process = False
+    n = 1
+    with open(filelist) as fh:
+        for line in fh:
+            fname = line.strip()
+            if n == start:
+                process = True
+            if n > end:
+                return
+            if process:
+                yield (n, fname)
+            n += 1
+
+
+def ensure_directory(*fnames):
+    """Ensure the directory part of all file names exists."""
+    for fname in fnames:
+        directory = os.path.split(fname)[0]
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+
